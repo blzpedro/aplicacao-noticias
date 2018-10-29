61ae6dec1cb9a4d3bf242e7704926c101baf9d95
Pedro Melo <phenriqmelo99@gmail.com>
2018-10-29 20:38:59 +0000

    Terceiro commit

--- test/controllers/inscritos_controller_test.rb
+++ test/controllers/inscritos_controller_test.rb
@@ -15,14 +15,6 @@ class InscritosControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create inscrito" do
-    assert_difference('Inscrito.count') do
-      post inscritos_url, params: { inscrito: { email: @inscrito.email } }
-    end
-
-    assert_redirected_to inscrito_url(Inscrito.last)
-  end
-
   test "should show inscrito" do
     get inscrito_url(@inscrito)
     assert_response :success
@@ -32,17 +24,5 @@ class InscritosControllerTest < ActionDispatch::IntegrationTest
     get edit_inscrito_url(@inscrito)
     assert_response :success
   end
-
-  test "should update inscrito" do
-    patch inscrito_url(@inscrito), params: { inscrito: { email: @inscrito.email } }
-    assert_redirected_to inscrito_url(@inscrito)
-  end
-
-  test "should destroy inscrito" do
-    assert_difference('Inscrito.count', -1) do
-      delete inscrito_url(@inscrito)
-    end
-
-    assert_redirected_to inscritos_url
-  end
+  
 end
--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -3,7 +3,16 @@ require 'test_helper'
 class MainControllerTest < ActionDispatch::IntegrationTest
   test "should get noticias" do
     get main_noticias_url
+    assert_select "h1", "Noticias"
+    assert_select "p", 2
     assert_response :success
+  end
+
+  test "should get premium" do
+      get main_premium_url
+      assert_select "a", "Voltar"
+      assert_select "h3", 3 || "p", 3
+      assert_response :success
   end
 
 end
