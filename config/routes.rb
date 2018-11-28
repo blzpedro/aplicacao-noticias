05811c603a8265f5068c8c33319802bbff12abc5
Pedro Melo <phenriqmelo99@gmail.com>
2018-11-28 13:42:52 +0000

    Pedro - Configurando rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'main/premium'
-  resources :inscritos
-  get 'main/noticias'
+  get 'premium' => 'main#premium', as: 'main_premium' 
+  get    "/inscritos"          => "inscritos#index"    ,as: "inscritos"
+  post   "/inscritos"          => "inscritos#create"   ,as: ""
+  get    "/inscritos/:id/edit" => "inscritos#edit"     ,as: "edit_inscrito"
+  get    "/inscritos/:id"      => "inscritos#show"     ,as: "inscrito"
+  patch  "/inscritos/:id"      => "inscritos#update"   ,as: ""
+  put    "/inscritos/:id"      => "inscritos#update"   ,as: ""
+  delete "/inscritos/:id"      => "inscritos#destroy"  ,as: ""
+  get '/cadastro' => 'inscritos#new', as: 'new_inscrito'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "main#noticias"
-  get 'main/sobre'
+  get 'sobre' => 'main#sobre', as: 'main_sobre'
   get 'futebol' => 'sessions#futebol', as: 'sessions_futebol'
   get 'luta' => 'sessions#luta', as: 'sessions_luta'
   get 'volei' => 'sessions#volei', as: 'sessions_volei'
